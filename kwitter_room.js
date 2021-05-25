eae1ba043ac962a644fc0d856ae5a9bb0438d72a
Pratty1205 <78639572+Pratty1205@users.noreply.github.com>
2021-05-25 22:43:11 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCwquwDda1Q5jcxIyWYVIkmsGo60YuAreU",
+    authDomain: "let-s-chat-82f60.firebaseapp.com",
+    projectId: "let-s-chat-82f60",
+    storageBucket: "let-s-chat-82f60.appspot.com",
+    messagingSenderId: "117704036504",
+    appId: "1:117704036504:web:5db7af4021db245aaf40ed"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
